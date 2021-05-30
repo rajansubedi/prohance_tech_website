c72e47f9422e44b33a20fc4fbedacf1dc5146be3
Rajan Subedi <rajan@Rajans-MacBook-Pro.local>
2021-05-30 21:54:14 +0545

    added php mailer

--- contact.php
+++ contact.php
@@ -53,15 +53,15 @@ License: https://webthemez.com/license
           <li><a href="about.html">About Us</a></li>
           <li><a href="services.html">Services</a></li>
           <li><a href="portfolio.html">Portfolio</a></li>
-          <li class="menu-has-children"><a href="">Dropdown</a>
+          <!-- <li class="menu-has-children"><a href="">Dropdown</a>
             <ul>
               <li><a href="#">Link Item 1</a></li>
               <li><a href="#">Link Item 3</a></li>
               <li><a href="#">Link Item 4</a></li>
               <li><a href="#">Link Item 5</a></li>
             </ul>
-          </li>
-          <li class="menu-active"><a href="contact.html">Contact</a></li>
+          </li> -->
+          <li class="menu-active"><a href="contact.php">Contact</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
     </div>
@@ -72,7 +72,7 @@ License: https://webthemez.com/license
   ============================-->
   <section id="innerBanner"> 
     <div class="inner-content">
-      <h2><span>Contact</span><br>We create the opportunities!</h2>
+      <h2><span>Contact</span></h2>
       <div> 
       </div>
     </div> 
@@ -95,7 +95,7 @@ License: https://webthemez.com/license
             <div class="contact-address">
               <i class="ion-ios-location-outline"></i>
               <h3>Address</h3>
-              <address>MN-12 Lincon Street, NewYork 12356, USA</address>
+              <address>3785 Post Oak Blvd, Euless, TX, 76040 USA</address>
             </div> 
             <div class="contact-phone">
               <i class="ion-ios-telephone-outline"></i>
@@ -105,7 +105,7 @@ License: https://webthemez.com/license
             <div class="contact-email">
               <i class="ion-ios-email-outline"></i>
               <h3>Email</h3>
-              <p><a href="mailto:info@example.com">name@website.com</a></p>
+              <p><a href="mailto:info@prohancetech.com">info@prohancetech.com</a></p>
             </div> 
 		 </div>
 		 <div class="col-lg-7">
@@ -113,12 +113,12 @@ License: https://webthemez.com/license
         <div class="form"> 
 		  
 		   <!-- Form itself -->
-          <form name="sentMessage" class="well" id="contactForm"  novalidate> 
+          <form name="sentMessage" class="well" id="contactForm"  novalidate action="" method="post"> 
 		 <div class="control-group">
                    <div class="form-group">
 			<input type="text" class="form-control" 
 			   	   placeholder="Full Name" id="name" required
-			           data-validation-required-message="Please enter your name" />
+			           data-validation-required-message="Please enter your name" name="full_name" />
 			  <p class="help-block"></p>
 
 		   </div>
@@ -127,7 +127,7 @@ License: https://webthemez.com/license
                   <div class="controls">
 			<input type="email" class="form-control" placeholder="Email" 
 			   	            id="email" required
-			   		   data-validation-required-message="Please enter your email" />
+			   		   data-validation-required-message="Please enter your email"  name="email"/>
 		</div>
 	    </div> 	
 			  
@@ -137,11 +137,11 @@ License: https://webthemez.com/license
                        placeholder="Message" id="message" required
 		       data-validation-required-message="Please enter your message" minlength="5" 
                        data-validation-minlength-message="Min 5 characters" 
-                        maxlength="999" style="resize:none"></textarea>
+                        maxlength="999" style="resize:none" name="message"></textarea>
 		</div>
                </div> 		 
 	     <div id="success"> </div> <!-- For success/fail messages -->
-	    <button type="submit" class="btn btn-primary pull-right">Send</button><br />
+	    <input type="submit" class="btn btn-success pull-right pr-5 pl-5" name="submit" value="Send"><br />
           </form>
         </div>
 
@@ -153,126 +153,127 @@ License: https://webthemez.com/license
       </div>
 
       <div class="container mb-4 map">
-	  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d387191.33750346623!2d-73.979681!3d40.6974881!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sin!4v1541477355474" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe> 
+	  <iframe src="https://maps.google.com/maps?q=3785%20post%20oak%20Blvd,%20Euless,%20TX,%2076040%20USA&t=&z=7&ie=UTF8&iwloc=&output=embed" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe> 
       </div>
  
     </section><!-- #contact -->
 
   </main>
 
-    <!--==========================
+<!--==========================
     Footer
   ============================-->
   <footer>
     <div class="footer-wrap">
-    <div class="container first_class">
+      <div class="container first_class">
         <div class="row">
           <div class="col-md-4 col-sm-6">
             <h3>BE THE FIRST TO KNOW</h3>
-            <p>Get all the latest information on  Triedge Services, Events, Jobs
+            <p>Get all the latest information on Triedge Services, Events, Jobs
               and Fairs. Sign up for our newsletter today.</p>
           </div>
           <div class="col-md-4 col-sm-6">
-          <form class="newsletter">
-             <input type="text" placeholder="Email Address"> 
-                                                          <button class="newsletter_submit_btn" type="submit"><i class="fa fa-paper-plane"></i></button>	
-          </form>
-          
+            <form class="newsletter">
+              <input type="text" placeholder="Email Address">
+              <button class="newsletter_submit_btn" type="submit"><i class="fa fa-paper-plane"></i></button>
+            </form>
+
           </div>
           <div class="col-md-4 col-sm-6">
-          <div class="col-md-12">
-            <div class="standard_social_links">
-          <div>
-            <li class="round-btn btn-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
-  
-            </li>
-            <li class="round-btn btn-linkedin"><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
-  
-            </li>
-            <li class="round-btn btn-twitter"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-  
-            </li>
-            <li class="round-btn btn-instagram"><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
-  
-            </li>
-            <li class="round-btn btn-whatsapp"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
-  
-            </li>
-            <li class="round-btn btn-envelop"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-  
-            </li>
-          </div>
-        </div>	
-          </div>
+            <div class="col-md-12">
+              <div class="standard_social_links">
+                <div>
+                  <li class="round-btn btn-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
+
+                  </li>
+                  <li class="round-btn btn-linkedin"><a href="#"><i class="fab fa-linkedin-in"
+                        aria-hidden="true"></i></a>
+
+                  </li>
+                  <li class="round-btn btn-twitter"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+
+                  </li>
+                  <li class="round-btn btn-instagram"><a href="#"><i class="fab fa-instagram"
+                        aria-hidden="true"></i></a>
+
+                  </li>
+                  <li class="round-btn btn-whatsapp"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+
+                  </li>
+                  <li class="round-btn btn-envelop"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+
+                  </li>
+                </div>
+              </div>
+            </div>
             <div class="clearfix"></div>
-          <div class="col-md-12"><h3 style="text-align: right;">Stay Connected</h3></div>
+            <div class="col-md-12">
+              <h3 style="text-align: right;">Stay Connected</h3>
+            </div>
           </div>
         </div>
-    </div>
+      </div>
       <div class="second_class">
         <div class="container second_class_bdr">
-        <div class="row">
-          <div class="col-md-4 col-sm-6">
-  
-            <div class="footer-logo"><img src="img/logo.png" alt="logo" width="280">
+          <div class="row">
+            <div class="col-md-4 col-sm-6">
+
+              <div class="footer-logo"><img src="img/logo.png" alt="logo" width="280">
+              </div>
+              <p>Our mission is to provide innovative software solutions for excellence and enterprise compliance.</p>
             </div>
-            <p>Your one-stop career platform to find Jobs, Internships, Professional Trainings, Projects, and Volunteering Opportunities.</p>
+            <div class="col-md-2 col-sm-6">
+              <h3>Quick LInks</h3>
+              <ul class="footer-links">
+                <li><a href="#">Home</a>
+                </li>
+                <li><a href="#">About us</a>
+                </li>
+                <li><a href="#">Portfolio</a>
+                </li>
+                <li><a href="#">Contact Us</a>
+                </li>
+                <li><a href="#" target="_blank">Terms &amp; Conditions</a>
+                </li>
+                <li><a href="#" target="_blank">Privacy Policy</a>
+                </li>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <h3>OUR SERVICES</h3>
+              <ul class="footer-category">
+                <li><a href="#">Sofware Development</a></li>
+                <li><a href="#">Mobile App Development</a></li>
+                <li><a href="#">Website Development</a></li>
+                <li><a href="#">IT Consulting</a></li>
+                <li><a href="#">UX-UI Design</a></li>
+                <li><a href="#">Digital Marketing</a></li>
+
+              </ul>
+              <div class="clearfix"></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+              <h3 class="mt-lg-0 mt-sm-4">Location</h3>
+              <p>3785 Post Oak Blvd, Euless, TX, 76040 USA</p>
+              <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+              <p><i class="fa fa-envelope-o mr-3"></i>info@prohancetech.com</p>
+            </div>
           </div>
-          <div class="col-md-2 col-sm-6">
-            <h3>Quick  LInks</h3>
-            <ul class="footer-links">
-              <li><a href="#">Home</a>
-              </li>
-              <li><a href="#">About us</a>
-              </li>
-              <li><a href="#">Portfolio</a>
-              </li>
-              <li><a href="#">Contact Us</a>
-              </li>
-              <li><a href="#" target="_blank">Terms &amp; Conditions</a>
-              </li>
-              <li><a href="#" target="_blank">Privacy Policy</a>
-              </li>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <h3>OUR SERVICES</h3>
-            <ul class="footer-category">
-              <li><a href="#">Fresher Jobs</a>
-              </li>
-              <li><a href="#">InternEdge - Internships &amp; Projects </a>
-              </li>
-              <li><a href="#">Resume Edge - Resume Writing Services</a>
-              </li>
-              <li><a href="#">Readers Galleria - Curated Library</a>
-              </li>
-              <li><a href="#">Trideus - Campus Ambassadors</a>
-              </li>
-            </ul>
-            <div class="clearfix"></div>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <h3 class="mt-lg-0 mt-sm-4">Location</h3>
-      <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
-      <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
-      <p><i class="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
-          </div>
+
         </div>
-        
       </div>
-      </div>
-      
+
       <div class="row">
-        
+
         <div class="container-fluid">
-        <div class="copyright"> Copyright 2021 | All Rights Reserved by Prohance Technologies LLC</div>
+          <div class="copyright"> Copyright 2021 | All Rights Reserved by Prohance Technologies LLC</div>
         </div>
-        
+
       </div>
     </div>
-    
-    </footer>
+
+  </footer>
 
   <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
   <!-- Font Awesome 5 links-->
@@ -294,3 +295,48 @@ License: https://webthemez.com/license
 
 </body>
 </html>
+
+
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+    
+if (isset($_POST['submit'])) {
+    $mail = new PHPMailer();                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'raj.sub505@gmail.com';                 // SMTP username
+        $mail->Password = 'B@ltimore1';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                           // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TLS port to connect to
+
+        //Recipients
+        $mail->setFrom($_POST['email']);
+        $mail->addAddress('rajan.subedi32@gmail.com');     // Add a recipient
+
+
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'This is Subject Line';
+        $mail->Body    = $_POST['message'];
+
+        $mail->send();
+        echo 'Message has been sent';
+     
+        exit();
+    } catch (Exception $e) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    }
+}
+    ?>
+
